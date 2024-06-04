dc3c0cdfab5fa2ffa76136688ce4fc41bfd005d4
thedakeen <thedakeen@gmail.com>
2024-06-04 15:40:51 +0500

    [product]build: Product service

--- Makefile
+++ Makefile
@@ -2,3 +2,9 @@
 protoc/auth:
 	@echo 'Updating proto...'
 	protoc -I protos protos/auth.proto --go_out=services/auth/gen/auth --go_opt=paths=source_relative --go-grpc_out=services/auth/gen/auth --go-grpc_opt=paths=source_relative
+	protoc -I protos protos/auth.proto --go_out=services/product/gen/auth --go_opt=paths=source_relative --go-grpc_out=services/product/gen/auth --go-grpc_opt=paths=source_relative
+
+.PHONY: protoc/product
+protoc/product:
+	@echo 'Updating proto...'
+	protoc -I protos protos/product.proto --go_out=services/product/gen/product --go_opt=paths=source_relative --go-grpc_out=services/product/gen/product --go-grpc_opt=paths=source_relative
--- services/product/cmd/product/main.go
+++ services/product/cmd/product/main.go
@@ -1,1 +1,67 @@
 package main
+
+import (
+	"context"
+	"golang.org/x/exp/slog"
+	"os"
+	"product/internal/config"
+	"product/internal/lib/logger/handlers/slogpretty"
+	"product/pkg/storage/mongo"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	mongo.MustStart(cfg.StoragePath)
+	defer func(ctx context.Context) {
+		err := mongo.Close(ctx)
+		if err != nil {
+
+		}
+	}(context.Background())
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
